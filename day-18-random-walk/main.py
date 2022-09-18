ddbc24b5b443df18759f30d3de1f85d0898681ff
Emmanuel Thomas <m.emmanuel.thomas@gmail.com>
2022-09-18 12:57:45 -0500

    use more random colors in random walk

--- day-18-random-walk/main.py
+++ day-18-random-walk/main.py
@@ -1,16 +1,22 @@
 import random
+import turtle
 from turtle import Turtle, Screen
 
 tim = Turtle()
+turtle.colormode(255)
 
-colors = ["orange red", "magenta", "yellow", "green yellow", "deep pink", "deep sky blue", "blue violet", "dark green",
-          "dark blue", "dark goldenrod"]
+def rand_color():
+    red = random.randint(0, 255)
+    green = random.randint(0, 255)
+    blue = random.randint(0, 255)
+    color_tuple = (red, green, blue)
+    return color_tuple
 
 tim.pensize(5)
 directions = [0, 90, 180, 270]
 
 for _ in range(200):
-    tim.pencolor(random.choice(colors))
+    tim.pencolor(rand_color())
     tim.forward(30)
     tim.setheading(random.choice(directions))
 
