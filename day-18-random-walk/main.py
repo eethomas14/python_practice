83f622f62112cab206be19a4be6e5cf80c257669
Emmanuel Thomas <m.emmanuel.thomas@gmail.com>
2022-09-18 10:56:36 -0500

    turtle random walk random color

--- day-18-random-walk/main.py
+++ day-18-random-walk/main.py
@@ -1,5 +1,4 @@
 import random
-import turtle
 from turtle import Turtle, Screen
 
 tim = Turtle()
@@ -7,18 +6,13 @@ tim = Turtle()
 colors = ["orange red", "magenta", "yellow", "green yellow", "deep pink", "deep sky blue", "blue violet", "dark green",
           "dark blue", "dark goldenrod"]
 
-def draw_figure(sides):
-    angle = 360 / sides
+tim.pensize(5)
+directions = [0, 90, 180, 270]
+
+for _ in range(200):
     tim.pencolor(random.choice(colors))
-    for _ in range(sides):
-        tim.fd(50)
-        tim.right(angle)
-
-i = 3
-
-while i <= 10:
-    draw_figure(i)
-    i += 1
+    tim.forward(30)
+    tim.setheading(random.choice(directions))
 
 screen = Screen()
 screen.exitonclick()
