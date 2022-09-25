114ad424e104a4192b95758bfa755cb30df0c341
Emmanuel Thomas <m.emmanuel.thomas@gmail.com>
2022-09-25 03:42:34 -0500

    add more functions and listeners

--- turtle-sketcher/main.py
+++ turtle-sketcher/main.py
@@ -4,10 +4,30 @@ turt = Turtle()
 screen = Screen()
 
 
-def move_forwards():
+def move_forward():
     turt.forward(10)
+
+def move_backward():
+    turt.backward(10)
+
+def move_right():
+    turt.right(10)
+
+def move_left():
+    turt.left(10)
+
+def clear_screen():
+    turt.clear()
+    turt.penup()
+    turt.home()
+    turt.pendown()
 
 
 screen.listen()
-screen.onkey(key="space", fun=move_forwards)
+screen.onkey(key="w", fun=move_forward)
+screen.onkey(key="s", fun=move_backward)
+screen.onkey(key="a", fun=move_left)
+screen.onkey(key="d", fun=move_right)
+screen.onkey(key="c", fun=clear_screen)
+
 screen.exitonclick()
